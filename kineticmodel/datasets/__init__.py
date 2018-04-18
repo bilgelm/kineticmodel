c3bcdc1c98d83aa6ce7227f6eac3c2fe3b23bafe
bilgelm <murat.bilgel@nih.gov>
2018-04-18 15:26:17 -0400

    voxelwise and nipype tests added back

--- kineticmodel/datasets/__init__.py
+++ kineticmodel/datasets/__init__.py
@@ -6,3 +6,5 @@ refRegionMask_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                   'cereb.mni305.2mm.nii.gz')
 timing_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                            'info_tim.csv')
+
+print('datasets init defined pet4D_file:' + pet4D_file)
---
+++ tests/test_nipype.py
@@ -0,0 +1,43 @@
+# THIS NEEDS TO BE REVISITED AFTER IMPLEMENTING VOXELWISE WRAPPER TEST
+
+try:
+    import os
+    import unittest
+
+    from kineticmodel.nipype import KineticModel
+    from kineticmodel.datasets import pet4D_file, timing_file, refRegionMask_file
+    from nipype.pipeline.engine import Node
+
+    class TestKineticModelNipype(unittest.TestCase):
+        def setUp(self):
+            # read in image from examples/data
+            #self.pet4D_file = os.path.abspath('examples/data/input.mni305.2mm.sm6.nii.gz')
+            #self.pet4D_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+            #                               os.pardir,'examples/data/input.mni305.2mm.sm6.nii.gz')
+            #self.timing_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+            #                                os.pardir,'examples/data/info_tim.csv')
+            #self.refRegionMaskFile = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+            #                                      os.pardir,'examples/data/cereb.mni305.2mm.nii.gz')
+            self.pet4D_file = pet4D_file
+            self.timing_file = timing_file
+            self.refRegionMaskFile = refRegionMask_file
+
+            self.time_unit = 'min'
+            self.startActivity = 'flat'
+            self.weights = 'frameduration'
+            self.fwhm = 4
+
+        def test_nipype_srtm_zhou2003(self):
+            km = Node(interface=KineticModel(model='SRTM_Zhou2003',
+                                             timeSeriesImgFile=self.pet4D_file,
+                                             frameTimingCsvFile=self.timing_file,
+                                             refRegionMaskFile=self.refRegionMaskFile,
+                                             time_unit=self.time_unit,
+                                             startActivity=self.startActivity,
+                                             weights=self.weights,
+                                             fwhm=self.fwhm), name="km")
+            km.run()
+
+except ImportError:
+    print('Cannot perform kineticmodel.nipype tests. \
+           To carry out these tests, install kineticmodel using nipype option.')
